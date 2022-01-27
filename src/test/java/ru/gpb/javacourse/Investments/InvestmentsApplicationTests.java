a2f0a8546b41b5a40963723a73020b6cee5478c4
rich1e <v.richkov@gmail.com>
2022-01-27 15:41:23 +0300

    add maven dependencies

---
+++ src/test/java/ru/gpb/javacourse/Investments/InvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpb.javacourse.Investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
